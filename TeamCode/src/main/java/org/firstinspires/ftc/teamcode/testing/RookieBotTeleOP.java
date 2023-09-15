b045298def659c970811febc3c35e0692c97935f
Delta11225 <lcsdeltarobotics@gmail.com>
2023-09-15 11:07:07 -0400

    CenterStage Repository

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/RookieBotTeleOP.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class RookieBotTeleOP extends LinearOpMode {
+
+    private DcMotor motor0;
+    private DcMotor motor1;
+
+    @Override
+    public void runOpMode() {
+
+        motor0 = hardwareMap.get(DcMotor.class, "motor0");
+        motor1 = hardwareMap.get(DcMotor.class, "motor1");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            motor0.setPower(-this.gamepad1.right_stick_y);
+            motor1.setPower(-this.gamepad1.left_stick_y);
+            telemetry.addData("status", "running");
+            telemetry.update();
+        }
+    }
+}
