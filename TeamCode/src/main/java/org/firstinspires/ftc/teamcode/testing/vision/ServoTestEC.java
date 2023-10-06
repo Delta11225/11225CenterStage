a8c4bc173f7ea2509087bba4987ab7c332264ee8
Delta11225 <lcsdeltarobotics@gmail.com>
2023-10-06 09:20:13 -0400

    Servor Motors move according to gamepad buttons.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/vision/ServoTestEC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/vision/ServoTestEC.java
@@ -5,36 +5,34 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+
+
 @TeleOp
 public class ServoTestEC extends LinearOpMode {
-
-
-    private Servo Servo;
+    private Servo servoTest;
 
     @Override
+
     public void runOpMode() {
-
-        Servo = hardwareMap.get(Servo.class, "servo");
-
+        servoTest = hardwareMap.get(Servo.class, "servo");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
         waitForStart();
-
         while (opModeIsActive()) {
-
-            if (gamepad1.x){
-                Servo.setPosition(0);
-                telemetry.addData("servo left", "0");
-                telemetry.addData("servo right", "0.98");
-                telemetry.update();
+            if (gamepad1.x) {
+// move to 0 degrees.
+                servoTest.setPosition(0);
             }
-            if (gamepad1.b){
-                Servo.setPosition(0);
-                telemetry.addData("servo left", "0.19");
-                telemetry.addData("servo right", "0.77");
-                telemetry.update();
+            if (gamepad1.y) {
+// move to 0 degrees.
+                servoTest.setPosition(0.5);
             }
+            if (gamepad1.b) {
+// move to 180 degrees.
+                servoTest.setPosition(1);
+            }
+            telemetry.addData("Servo Position", servoTest.getPosition());
+            telemetry.update();
         }
     }
 }
