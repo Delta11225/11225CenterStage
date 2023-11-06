a846c4babdb96c6b5a1ceddd45abef614c0a736a
Delta11225 <lcsdeltarobotics@gmail.com>
2023-11-06 11:01:42 -0500

    Arm and collector positions tested and set

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/teleop/CSTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/teleop/CSTeleop.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
-public class CSTeleop extends LinearOpMode{
+public class CSTeleop extends LinearOpMode {
     // Claw open and close
     private Servo Clamp;
     private Servo Arm;
@@ -19,74 +19,43 @@ public class CSTeleop extends LinearOpMode{
     public void runOpMode() throws InterruptedException {
         // Claws open and close
         Clamp = hardwareMap.get(Servo.class, "clamp");
+        Arm = hardwareMap.get(Servo.class, "arm");
+
+        linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        linearSlide.setDirection(DcMotor.Direction.REVERSE);
+        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
 
-        while(opModeIsActive()){
-            if (gamepad2.b){
-                //clamp down
-                Clamp.setPosition(0);
-
-            }
-            if(gamepad2.a){
-                //clamp up
-               Clamp.setPosition(1);
-
-            }
-        }
-        // Potentially correct Arm and Linear Slide code
-        linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
-        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        linearSlide.setDirection(DcMotor.Direction.REVERSE);
-        linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        Arm = hardwareMap.get(Servo.class,"arm" );
-
-        // code needed for camera to display on FTC Dashboard
-        FtcDashboard dashboard = FtcDashboard.getInstance();
-        telemetry = dashboard.getTelemetry();
-        //FtcDashboard.getInstance().startCameraStream(webcam, 10);
-        telemetry.update();
-
-        Arm.setPosition(0);
-
-
-
-        waitForStart();
         while (opModeIsActive()) {
-            if (gamepad2.dpad_up&& linearSlide.getCurrentPosition() < 1900 ) {
-                linearSlide.setPower(0.5);
-            } else if (gamepad2.dpad_down && linearSlide.getCurrentPosition() > 0) {
-                linearSlide.setPower(-0.5);
-            } else {
-                linearSlide.setPower(0.0);
+            if (gamepad2.b) {
+                //clamp down
+                Clamp.setPosition(0.75);
+
             }
+            if (gamepad2.a) {
+                //clamp up
+                Clamp.setPosition(1);
 
             }
 
             if (gamepad2.dpad_right) {
                 //deploy position
-                Arm.setPosition(0.5);
-            }
-            else if (gamepad2.dpad_left) {
+                Arm.setPosition(0.35);
+            } else if (gamepad2.dpad_left) {
                 //collect position
-                Arm.setPosition(0);
+                Arm.setPosition(0.07);
             }
-
-            if(gamepad2.a &&linearSlide.getCurrentPosition() < 120){
-
-
-
-            }
-
-            telemetry.addData("encoder",linearSlide.getCurrentPosition());
-            telemetry.update();
         }
+        // Potentially correct Arm and Linear Slide code
 
 
     }
+}
 
 
