296ae3ccf2bd0b5cd6cbda9d7fcd2591f2d6e8d6
Delta11225 <lcsdeltarobotics@gmail.com>
2023-11-08 10:34:01 -0500

    Added ScissorLift class to teleop branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/teleop/ScissorLift.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.testing.teleop;
+
+// import stuff
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class ScissorLift extends LinearOpMode {
+   DcMotor scissorLift;
+   @Override
+   public void runOpMode() {
+      scissorLift = hardwareMap.get(DcMotor.class, "scissorLift");
+      waitForStart();
+      while (opModeIsActive()) {
+            scissorLift.setPower(-gamepad1.right_stick_y);
+         }
+      }
+   }
